e834ddcf17b6b7f8333205e1e1d6a4d19f0d5d07
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2022-04-27 19:31:14 +0200

    I've added a neural network model to the project, in particular I've implemented a Multi-layer Perceptron Classifier.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from sklearn.model_selection import learning_curve
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score,recall_score,f1_score,accuracy_score
 from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 
 
 
@@ -212,13 +213,36 @@ plt.show()
 
 
 #Cross validation SVM
-
 train_sizes, train_scores, test_scores =learning_curve(estimator=SVC(kernel='rbf',C=1.0,random_state=0, gamma=2),
                                                        X=X_train_dataset,
                                                        y=Y_train_dataset,
                                                        cv=10,
                                                        n_jobs=1)
 
+train_mean=np.mean(train_scores,axis=1)
+train_std=np.std(train_scores,axis=1)
+test_mean=np.mean(test_scores,axis=1)
+test_std=np.std(test_scores,axis=1)
+
+plt.plot(train_sizes,train_mean,color='blue',marker='o',markersize=5,label='training accuracy')
+plt.fill_between(train_sizes,train_mean+train_std,train_mean-train_std,alpha=0.15,color='blue')
+plt.plot(train_sizes,test_mean,color='green',linestyle='--',marker='s',markersize=5,label='validation accuracy')
+plt.fill_between(train_sizes,test_mean+test_std,test_mean-test_std,alpha=0.15, color='green')
+plt.grid()
+plt.xlabel('Number of training samplel')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.title("SVM Accuracy")
+plt.ylim([0.0,1.0])
+plt.show()
+
+#Multi-layer Perceptron Classifier
+train_sizes, train_scores, test_scores =learning_curve(estimator=MLPClassifier(solver='lbfgs',alpha=1e-5,
+                                                                               hidden_layer_sizes=(5,2),random_state=1,max_iter=5000),
+                                                       X=X_train_dataset,
+                                                       y=Y_train_dataset,
+                                                       cv=10,
+                                                       n_jobs=1)
 train_mean=np.mean(train_scores,axis=1)
 train_std=np.std(train_scores,axis=1)
 test_mean=np.mean(test_scores,axis=1)
